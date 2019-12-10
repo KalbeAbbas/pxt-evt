40aafec8b6b11ccd5738e67a76a836c8b9f7e756
KalbeAbbas <kalbeabbas142@gmail.com>
2019-12-10 21:05:57 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -1,13 +1,4 @@
 //% color=190 weight=100 icon="\uf1ec" block="Event Blocks"
 namespace evt_test {
-    //% mutate=objectdestructuring
-    //% mutateText="My Arguments"
-    //% mutatePropertyEnum="argNames"
-    // ...
-    export function addSomeEventHandler(arg1: string) {
-        control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
-            basic.showString(arg1)
-        })
 
-    };
 }
