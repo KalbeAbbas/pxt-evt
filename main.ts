4246ee892e898f3f86d17bbc900603c6f0fa617f
KalbeAbbas <kalbeabbas142@gmail.com>
2019-12-10 21:01:56 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+namespace evt_test {
+    //% mutate=objectdestructuring
+    //% mutateText="My Arguments"
+    //% mutatePropertyEnum="argNames"
+    // ...
+    export function addSomeEventHandler(arg1: string) {
+        control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
+            basic.showString(arg1)
+        })
+
+    };
+}
