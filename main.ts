72f9106a76f65014ed88024d1ee14c05c2d16c58
KalbeAbbas <kalbeabbas142@gmail.com>
2019-12-11 08:23:32 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 //% color=190 weight=100 icon="\uf1ec" block="Event Blocks"
 namespace evt_test {
+    let num = 0
+
     //% block
     export function helloWorld() {
 
+    }
+
+    /**
+     * Events can have arguments before the handler
+     */
+    //% block="on loop $topic"
+    export function onLoop(topic: string, handler: () => void)
+    {
+        while(true)
+        {
+            basic.showNumber(num)
+            basic.pause(100)
+            num++
+        }
     }
 
     /**
