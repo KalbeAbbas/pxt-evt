30387236ae556c3496a607778126cca90c95ef72
KalbeAbbas <kalbeabbas142@gmail.com>
2019-12-11 11:42:23 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -26,11 +26,7 @@ namespace evt_test {
      */
     //% block="on Subscribe topic $topic"
     export function onSubscribe(topic: string, handler: () => void) {
-
-        control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
-            basic.showString(topic)
-
-        })
+        basic.showString(topic)
 
     }
 
